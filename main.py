5d80955300ed41ab93fd10874e1729d13f0beb0a
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-10-27 19:54:41 -0400

    added check_number_of_questions function to main.py which checks how many questions the player wants to answer.

--- main.py
+++ main.py
@@ -2,14 +2,29 @@
 and time our answers.
 """
 from quiz import Quiz
-
+    
+    
+def check_number_of_questions():
+    # check if number
+    while True:
+        try:
+            num_of_questions = int(input('How many questions do you want?'))
+        except ValueError:
+            print('Enter a valid whole number')
+            continue
+        break  # breaks if valid number is given.
+    return num_of_questions
+   
+ 
 # create class to keep track of quiz scores over time?
 def main():
     while True:
         quiz_prompt = input('Do you want a math quiz? y/n')
         if quiz_prompt.lower()[0] == 'y':
-            quiz = Quiz()  # start quiz
+            num_of_questions = check_number_of_questions()
+            quiz = Quiz(num_of_questions)  # start quiz
             quiz.take_quiz()
+            
         elif quiz_prompt.lower()[0] == 'n':
             print('Ok. Goodbye')
             break
