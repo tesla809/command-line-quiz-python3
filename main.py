2d9ac8d04f2c5ce8d05d2fe5d1267017f6405bbb
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-10-30 14:27:37 -0400

    moved check_number_of_questions to quiz.py and renamed it ask_number_of_questions. Simplfied main.py

--- main.py
+++ main.py
@@ -2,18 +2,6 @@
 and time our answers.
 """
 from quiz import Quiz
-    
-    
-def check_number_of_questions():
-    # check if number
-    while True:
-        try:
-            num_of_questions = int(input('How many questions do you want?'))
-        except ValueError:
-            print('Enter a valid whole number')
-            continue
-        break  # breaks if valid number is given.
-    return num_of_questions
    
  
 # create class to keep track of quiz scores over time?
@@ -21,10 +9,8 @@ def main():
     while True:
         quiz_prompt = input('Do you want a math quiz? y/n')
         if quiz_prompt.lower()[0] == 'y':
-            num_of_questions = check_number_of_questions()
-            quiz = Quiz(num_of_questions)  # start quiz
-            quiz.take_quiz()
-            
+            quiz1 = Quiz() 
+            quiz1.take_quiz() # start quiz
         elif quiz_prompt.lower()[0] == 'n':
             print('Ok. Goodbye')
             break
